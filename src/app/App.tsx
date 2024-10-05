5751f28861dc463ffd59fb1f50b20193930d3ceb
Mina Shokouhi <minaashokouhi@gmail.com>
2024-10-05 10:07:24 +0300

    1-setup react-query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MainContent } from '../modules'
+
+// Create a client
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <MainContent />
+    </QueryClientProvider>
   )
 }
 
